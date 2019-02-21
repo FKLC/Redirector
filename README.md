d17901942c4f1df852a012dc40511399eae99eb7
Fatih Kılıç <m.fatihklc0@gmail.com>
2019-02-21 18:57:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# MentionRedirector Bot
+MentionRedirector Bot is a discord server moderation bot. It helps you to keep clean your chat with removing and sending DMs to users if message content is only mention of users.
+
+
+## How it works?
+It removes all mentions from message and check if there is anything left also checks for any embed if the both are false it deletes messages and send DM's to mentioned users with server and channel link in it.
+
+## Invite From [Here](https://discordapp.com/oauth2/authorize?client_id=548145751251877888&scope=bot&permissions=8192)
---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from os import environ
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+
+@client.event
+async def on_message(message):
+    for mention in message.raw_mentions:
+        message.content = message.content.replace(
+            f"<@{mention}>", ""
+        )
+        message.content = message.content.replace(
+            f"<@!{mention}>", ""
+        )
+    if not message.content.strip() and not message.embeds:
+        await message.delete()
+        for mentioned_user in message.mentions:
+            await mentioned_user.send(
+                f"{message.author.display_name} mentioned you on https://discordapp.com/channels/{message.guild.id}/{message.channel.id}/"
+            )
+
+
+client.run(environ.get("TOKEN"), bot=True)
